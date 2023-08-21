05f17f4f4849b8166219c6bd78864d3cfc80c6d6
Maaz Javed <maaz.ravian@gmail.com>
2023-08-22 00:39:22 +0500

    Added VM for pagination

---
+++ MVC-CRUD/Models/StudentListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Models
+{
+    public class StudentListViewModel
+    {
+        
+            public List<student> Students { get; set; }
+            public int CurrentPage { get; set; }
+            public int TotalPages { get; set; }
+
+    }
+}
